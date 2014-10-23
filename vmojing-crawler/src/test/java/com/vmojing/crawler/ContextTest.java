ad11d5b752d52d1dfc5d914965e49c2ba2529b8e
v11 <442629928@qq.com>
2014-10-23 15:12:33 +0800

    1
    1

--- vmojing-crawler/src/test/java/com/vmojing/crawler/ContextTest.java
+++ vmojing-crawler/src/test/java/com/vmojing/crawler/ContextTest.java
@@ -1,5 +1,24 @@
 package com.vmojing.crawler;
 
-public class ContextTest {
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.vmojing.mongodb.domain.Topic;
+import com.vmojing.mongodb.repository.BasicRepository;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {RootConfiguration.class})
+public class ContextTest {
+	@Autowired
+	@Qualifier("topicDao")
+	BasicRepository<Topic> topicDao;
+	@Test
+	public void testContext(){
+		System.out.println(topicDao.countAndClose(topicDao.findByAll()));
+	}
 }
